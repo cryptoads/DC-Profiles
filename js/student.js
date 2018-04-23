6f050c320ef6980c58713903b1e529aa76caf15e
Chris Michels <chrisjohnmichels@gmail.com>
2018-04-22 22:08:19 -0400

    fixed the modal centering

--- js/student.js
+++ js/student.js
@@ -7,6 +7,7 @@ var createStudents = function(){$.get('https://s3.amazonaws.com/dc-profiles/Stud
         //wipe students div
         $('.students').empty();
         var finalHtml = "";
+        var modalHtml = "";
         var searchFilter = "";
         //check for default search string
         if($('.search').val() !== 'Search for students'){
@@ -27,24 +28,23 @@ var createStudents = function(){$.get('https://s3.amazonaws.com/dc-profiles/Stud
             finalHtml+='<div class="card my-2 mx-3" style="width: 18rem;"><div class="card-body d-flex flex-column"><h5 class="card-title">'+student.fullName;
             finalHtml+= '</h5><h6 class="card-subtitle mb-2 text-muted">'+student.missionStatement+'</h6><p class ="card-text">'+ student.fullBio;
             finalHtml+= '</p><button class ="btn btn-primary mt-auto" type="button" data-toggle="modal" data-target="#'+student.id+'">Full Information</button></div></div>';
-            finalHtml+='<div class="modal fade" id="'+student.id+'" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">';
-            finalHtml+='<div class="modal-dialog modal-dialog-centered" role="document">';
-            finalHtml+='<div class="modal-content">';
-            finalHtml+='<div class="modal-header d-block">';
-            finalHtml+='<h5 class="modal-title" id="exampleModalLongTitle">'+student.fullName+'</h5>';
-            finalHtml+='<h6 class="modal-subtitle text-muted">'+student.missionStatement+'</h6>';
-            finalHtml+='<button type="button" class="close" data-dismiss="modal" aria-label="Close">';
-            finalHtml+='<span aria-hidden="true">&times;</span>';
-            finalHtml+='</button></div>';
-            finalHtml+='<div class="modal-body d-block">';
-            finalHtml+=''+student.portfolioUrl+'<br>'+student.githubUrl+'<br>'+student.linkedinUrl+'<br>'+student.fullBio+'<br>'+student.email+'<br>'+'</div>';
-            finalHtml+='<div class="modal-footer">';
-            finalHtml+='<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>';
-            finalHtml+='</div></div></div></div>';
+
+            modalHtml+='<div class="modal fade" id="'+student.id+'" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">';
+            modalHtml+='<div class="modal-dialog modal-dialog-centered" role="document">';
+            modalHtml+='<div class="modal-content">';
+            modalHtml+='<div class="modal-header d-block">';
+            modalHtml+='<h5 class="modal-title" id="exampleModalLongTitle">'+student.fullName+'</h5>';
+            modalHtml+='<h6 class="text-muted">'+student.missionStatement+'</h6></div>';
+            modalHtml+='<div class="modal-body">';
+            modalHtml+=''+student.portfolioUrl+'<br>'+student.githubUrl+'<br>'+student.linkedinUrl+'<br>'+student.fullBio+'<br>'+student.email+'<br>'+'</div>';
+            modalHtml+='<div class="modal-footer">';
+            modalHtml+='<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>';
+            modalHtml+='</div></div></div></div>';
         });
 
         //send the cards to the page
         $('.students').append(finalHtml);
+        $('.container').append(modalHtml);
     });
 };
 createStudents();
